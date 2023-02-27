8d6c0b808f5071d751f6ef3eba30e3f055743d9c
Oscarrscso <118562321+Oscarrscso@users.noreply.github.com>
2023-02-27 13:46:00 +0000

    you can draw now

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const canvas = document.getElementById('canvas');
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-  
+const palleteBTNS = document.querySelectorAll('.pallete-btn');
 
-  const drawBox = (x, y) =>
-  {
+let drawColor = 'red';
 
-    let randomRed = Math.floor(Math.random() * 256);
-    let randomBlue = Math.floor(Math.random() * 256);
-    let randomGreen = Math.floor(Math.random() * 256);
-
-    let randomSize = Math.floor(Math.random() * 50);
-
+const drawBox = (x, y) => {
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = `rgb(${randomRed}, ${randomBlue}, ${randomGreen})`;
-    ctx.fillRect(x, y, randomSize, randomSize);
-    
+    ctx.fillStyle = drawColor;
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2*Math.PI);
+    ctx.fill();
   }
 
+let isDrawing = false;
 
-  canvas.addEventListener('click', (e) => {
-    drawBox(e.clientX, e.clientY);
+canvas.addEventListener('mousedown', e => isDrawing = true);
+canvas.addEventListener('mouseup', e => isDrawing = false);
+canvas.addEventListener('mouseout', e => isDrawing = false);
+
+canvas.addEventListener('mousemove', e => {
+  if (isDrawing) {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    if (x >= 0 && x <= canvas.width && y >= 0 && y <= canvas.height) {
+      drawBox(x, y);
+    }
+  }
+});
+
+palleteBTNS.forEach(btn => {
+  btn.style.backgroundColor = btn.id;
+  btn.addEventListener('click', e => {
+    drawColor = e.target.id;
+    palleteBTNS.forEach(btn => btn.classList.remove('activeColor'));
+    document.getElementById(e.target.id).classList.add('activeColor');
   });
-  
+});
+
